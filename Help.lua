87071101f76fd4972b628597a37ed3793c1e6d5b
Jkram143 <lpatikim01@gmail.com>
2025-08-28 04:56:44 +0800

    Update Help.lua

--- Help.lua
+++ Help.lua
@@ -1,11 +1,11 @@
--- Set your Discord invite link here:
-local discord = "https://discord.gg/95EkzpEPma"
+-- Set your facebook invite link here:
+local FACEBOOK = "https://www.facebook.com/jkram143"
 
 -- Copy the Discord link to clipboard ✨
-gg.copyText(discord)
+gg.copyText(FACEBOOK)
 
 -- Notify user of successful copy 🎊
-gg.alert("Discord Server Link Copied to Clipboard!✨\n📋 https://discord.gg/95EkzpEPma")
+gg.alert("FACEBOOK Link Copied to Clipboard!✨\n📋 https://discord.gg/95EkzpEPma")
 
 -- Encourage joining the community 🌟
-gg.alert("Join our vibrant Discord community for script support and engaging chats with fellow users! 🌟💬")
+gg.alert("Chat our vibrant FACEBOOK community for script support and engaging chats with fellow users! 🌟💬")
